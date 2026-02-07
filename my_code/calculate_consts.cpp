b7ef9fd43d7213d89c00dce4bcf3c2e528f860b3
Marco Gomez Hernandez <marco.gomezh@estudiante.uam.es>
2026-02-07 22:47:25 +0100

    a

--- my_code/calculate_consts.cpp
+++ my_code/calculate_consts.cpp
@@ -21,28 +21,50 @@
 #include "scaling.h"
 #include "scaling_utils.h"
 
-// Definición de dts (Input)
-static constexpr std::array<double, 144> dts = {
-    0.000500, 0.000600, 0.000700, 0.000800, 0.000900, 0.001000, 0.001100, 0.001200,
-    0.001300, 0.001400, 0.001500, 0.001600, 0.001800, 0.002000, 0.002200, 0.002500,
-    0.002800, 0.002900, 0.003000, 0.003100, 0.003200, 0.003300, 0.003400, 0.003500,
-    0.003600, 0.003700, 0.003800, 0.003900, 0.004000, 0.004100, 0.004200, 0.004300,
-    0.004400, 0.004500, 0.004600, 0.004700, 0.004800, 0.004900, 0.005000, 0.005100,
-    0.005200, 0.005400, 0.005600, 0.005800, 0.006000, 0.006200, 0.006400, 0.006600,
-    0.006800, 0.007000, 0.007200, 0.007400, 0.007700, 0.008000, 0.008300, 0.008600,
-    0.008900, 0.009200, 0.009600, 0.010000, 0.010400, 0.010900, 0.011400, 0.011900,
-    0.012500, 0.013100, 0.013800, 0.014600, 0.015400, 0.016300, 0.017300, 0.018500,
-    0.019900, 0.021500, 0.023300, 0.025500, 0.028100, 0.028400, 0.028700, 0.029000,
-    0.029400, 0.029800, 0.030200, 0.030600, 0.031000, 0.031400, 0.031800, 0.032200,
-    0.032600, 0.033000, 0.033400, 0.033900, 0.034400, 0.034900, 0.035400, 0.035900,
-    0.036400, 0.036900, 0.037400, 0.038000, 0.038600, 0.039200, 0.039800, 0.040400,
-    0.041000, 0.041700, 0.042400, 0.043100, 0.043800, 0.044500, 0.045300, 0.046100,
-    0.046900, 0.047700, 0.048600, 0.049500, 0.050400, 0.051400, 0.052400, 0.053400,
-    0.054500, 0.055600, 0.056800, 0.058000, 0.059300, 0.060600, 0.062000, 0.063400,
-    0.064900, 0.066500, 0.068200, 0.069900, 0.071700, 0.073600, 0.075600, 0.077700,
-    0.079900, 0.082300, 0.084800, 0.087500, 0.090300, 0.093300, 0.096500, 0.100000};
+namespace ConstCalculatorConfig
+{
+    // Definición de dts (Input)
+    static constexpr std::array<double, 144> DTS = {
+        0.000500, 0.000600, 0.000700, 0.000800, 0.000900, 0.001000, 0.001100, 0.001200,
+        0.001300, 0.001400, 0.001500, 0.001600, 0.001800, 0.002000, 0.002200, 0.002500,
+        0.002800, 0.002900, 0.003000, 0.003100, 0.003200, 0.003300, 0.003400, 0.003500,
+        0.003600, 0.003700, 0.003800, 0.003900, 0.004000, 0.004100, 0.004200, 0.004300,
+        0.004400, 0.004500, 0.004600, 0.004700, 0.004800, 0.004900, 0.005000, 0.005100,
+        0.005200, 0.005400, 0.005600, 0.005800, 0.006000, 0.006200, 0.006400, 0.006600,
+        0.006800, 0.007000, 0.007200, 0.007400, 0.007700, 0.008000, 0.008300, 0.008600,
+        0.008900, 0.009200, 0.009600, 0.010000, 0.010400, 0.010900, 0.011400, 0.011900,
+        0.012500, 0.013100, 0.013800, 0.014600, 0.015400, 0.016300, 0.017300, 0.018500,
+        0.019900, 0.021500, 0.023300, 0.025500, 0.028100, 0.028400, 0.028700, 0.029000,
+        0.029400, 0.029800, 0.030200, 0.030600, 0.031000, 0.031400, 0.031800, 0.032200,
+        0.032600, 0.033000, 0.033400, 0.033900, 0.034400, 0.034900, 0.035400, 0.035900,
+        0.036400, 0.036900, 0.037400, 0.038000, 0.038600, 0.039200, 0.039800, 0.040400,
+        0.041000, 0.041700, 0.042400, 0.043100, 0.043800, 0.044500, 0.045300, 0.046100,
+        0.046900, 0.047700, 0.048600, 0.049500, 0.050400, 0.051400, 0.052400, 0.053400,
+        0.054500, 0.055600, 0.056800, 0.058000, 0.059300, 0.060600, 0.062000, 0.063400,
+        0.064900, 0.066500, 0.068200, 0.069900, 0.071700, 0.073600, 0.075600, 0.077700,
+        0.079900, 0.082300, 0.084800, 0.087500, 0.090300, 0.093300, 0.096500, 0.100000};
+    static constexpr HindmarshRoseParams HR_PARAMS = {
+        3.281,  // e
+        0.0021, // mu
+        1.0,    // S
+        1.0,    // a
+        3.0,    // b
+        1.0,    // c
+        5.0,    // d
+        -1.6,   // xr
+        0.1     // vh
+    };
+    static constexpr HindmarshRoseState HR_INITIAL_STATE = {
+        -0.712841, // x
+        -1.93688,  // y
+        3.16568    // z
+    };
+    static constexpr double OBSERVATION_TIME = 2000.0;
+    static constexpr double MINMAX_DT = ConstCalculatorConfig::DTS[0];
+    static constexpr double STABILIZATION_TIME = 2000.0;
+}
 
-static constexpr size_t dts_size = dts.size();
+static constexpr size_t DTS_SIZE = ConstCalculatorConfig::DTS.size();
 
 // Add struct for neuron state
 struct HindmarshRoseState
@@ -67,9 +89,9 @@ struct HindmarshRoseParams
 };
 
 // Estructura de retorno
-struct CalcResult
+struct ConstCalcResult
 {
-    std::array<double, dts_size> pts;
+    std::array<double, DTS_SIZE> pts;
     double min;
     double max;
     std::vector<double> invalid_dts;
@@ -85,7 +107,7 @@ namespace ConstCalculatorConstants
  * N: Tamaño del array dts
  */
 template <typename NeuronType, typename NeuronParamsType, typename StateType, size_t N>
-CalcResult calculate_metrics(
+ConstCalcResult calculate_constants(
     NeuronModel model,
     const NeuronParamsType &config,
     const StateType &initial_state,
@@ -120,7 +142,7 @@ CalcResult calculate_metrics(
 
     NeuronType neuron(args);
 
-    CalcResult result;
+    ConstCalcResult result;
 
     // Set to initial state
     if (model == HINDMARSH_ROSE)
@@ -234,68 +256,52 @@ CalcResult calculate_metrics(
 
 int main()
 {
-    // 2. Configuración
-    HindmarshRoseParams hr_params = {
-        1.0,   // a
-        3.0,   // b
-        1.0,   // c
-        5.0,   // d
-        0.006, // r
-        4.0,   // s
-        -1.6,  // x_rest
-        3.0    // I (genera bursting)
-    };
-
-    HindmarshRoseState initial_state = {
-        -1.6,  // x
-        -10.0, // y
-        0.0    // z
-    };
-
-    // 3. Ejecución
-    double observation_time = 1000.0;   // Dimensionless observation time
-    double minmax_dt = dts[0];          // Use first dt for min/max calculation
-    double stabilization_time = 1000.0; // Stabilization time
-    std::cout << "Calculando constantes... Espere.\n";
-    auto result = calculate_metrics<DifferentialNeuronWrapper<SystemWrapper<HindmarshRoseModel<double>>, RungeKutta4>, HindmarshRoseParams, HindmarshRoseState>(HINDMARSH_ROSE, hr_params, initial_state, dts, observation_time, minmax_dt, stabilization_time);
+    ConstCalcResult result = calculate_constants<DifferentialNeuronWrapper<SystemWrapper<HindmarshRoseModel<double>>, RungeKutta4>, HindmarshRoseParams, HindmarshRoseState>(
+        NeuronModel::HINDMARSH_ROSE,
+        ConstCalculatorConfig::HR_PARAMS,
+        ConstCalculatorConfig::HR_INITIAL_STATE,
+        ConstCalculatorConfig::DTS,
+        ConstCalculatorConfig::OBSERVATION_TIME,
+        ConstCalculatorConfig::MINMAX_DT,
+        ConstCalculatorConfig::STABILIZATION_TIME);
 
     // 4. Salida Formateada
     std::cout << std::fixed << std::setprecision(6);
 
-    std::cout << "\n// Copiar y pegar en scaling.cpp / struct correspondiente:\n\n";
     std::cout << "static constexpr double min = " << result.min << ";\n";
     std::cout << "static constexpr double max = " << result.max << ";\n";
 
-    std::cout << "static constexpr std::array<double, " << dts_size << "> dts = {";
-    for (size_t i = 0; i < dts_size; ++i)
+    size_t ds_size_minus_1 = DTS_SIZE - 1;
+    std::cout << "static constexpr std::array<double, " << DTS_SIZE << "> dts = {";
+    for (size_t i = 0; i < DTS_SIZE; i++)
     {
         if (i % 8 == 0)
             std::cout << "\n    ";
-        std::cout << dts[i];
-        if (i < dts_size - 1)
+        std::cout << ConstCalculatorConfig::DTS[i];
+        if (i < ds_size_minus_1)
             std::cout << ", ";
     }
-    std::cout << "};\n";
+    std::cout << "\n};\n";
 
-    std::cout << "static constexpr std::array<double, " << dts_size << "> pts = {";
-    for (size_t i = 0; i < dts_size; ++i)
+    std::cout << "static constexpr std::array<double, " << DTS_SIZE << "> pts = {";
+    for (size_t i = 0; i < DTS_SIZE; i++)
     {
         if (i % 8 == 0)
             std::cout << "\n    ";
         std::cout << result.pts[i];
-        if (i < dts_size - 1)
+        if (i < ds_size_minus_1)
             std::cout << ", ";
     }
-    std::cout << "};\n";
+    std::cout << "\n};\n";
 
     if (!result.invalid_dts.empty())
     {
         std::cout << "Invalid dts (no bursts detected): ";
-        for (auto dt : result.invalid_dts)
+        for (double dt : result.invalid_dts)
         {
             std::cout << dt << " ";
         }
-        std::cout << std::endl;
+        std::cout << "\n";
     }
 
     return 0;
