4df198ecb3e5c8a69f7226b11b229ccbb960a333
Marco Gomez Hernandez <marco.gomezh@estudiante.uam.es>
2026-02-28 18:04:30 +0100

    a

--- my_code/genetic/fitness.hpp
+++ my_code/genetic/fitness.hpp
@@ -10,56 +10,55 @@ using namespace kfr;
 
 struct Individual
 {
-    ChemicalSynapsisParams params;
-    double fitness;
+  ChemicalSynapsisParams params;
+  double fitness;
 };
 
 struct ConstantSigFitnessVals
 {
-    univector<double> i_sig_centered_to_fit;
-    double i_sig_stddev_to_fit;
-    univector<double> ifast_sig_centered_to_fit;
-    double ifast_sig_stddev_to_fit;
-    univector<double> islow_sig_centered_to_fit;
-    double islow_sig_stddev_to_fit;
-    univector<double> smoothed_vpre_sig_to_fit;
-    double max_v_comp_distance;
+  univector<double> i_sig_centered_to_fit;
+  double i_sig_stddev_to_fit;
+  univector<double> ifast_sig_centered_to_fit;
+  double ifast_sig_stddev_to_fit;
+  univector<double> islow_sig_centered_to_fit;
+  double islow_sig_stddev_to_fit;
+  univector<double> smoothed_vpre_sig_to_fit;
+  double max_v_comp_distance;
 };
 
 struct SigBuffers
 {
-    univector<double> vpost_sig;
-    univector<double> i_sig;
-    univector<double> ifast_sig;
-    univector<double> islow_sig;
+  univector<double> vpost_sig;
+  univector<double> i_sig;
+  univector<double> ifast_sig;
+  univector<double> islow_sig;
 
-    univector<double> padded;
+  univector<double> padded;
 };
 
 ConstantSigFitnessVals calc_const_sig_fitness_vals(const univector<double> &vpre_sig,
-                                                         double min_val,
-                                                         double max_val,
-                                                         bool search_phase,
-                                                         size_t avg_smooth_points,
-                                                         size_t start_i,
-                                                         double pts_burst_real,
-                                                         SigBuffers &buffers,
-                                                         bool use_ifast,
-                                                         bool use_islow);
+                                                   double min,
+                                                   double max,
+                                                   bool search_phase,
+                                                   size_t avg_smooth_points,
+                                                   double pts_burst_real,
+                                                   SigBuffers &buffers,
+                                                   bool use_ifast,
+                                                   bool use_islow);
 
 template <typename Integrator, typename NeuronType, size_t N, ResetStateFunc<NeuronType> ResetStateFuncType, GetVFunc<NeuronType> GetVFuncType>
 void calc_fitnesses(ChemicalSynapsis<NeuronType, NeuronType, Integrator, double> &synapsis,
                     NeuronType &model_neur,
                     std::array<Individual, N> &individuals,
                     const ScaledSigResult &scaled_result,
-                    const ConstantSigFitnessVals &stats1,
+                    const ConstantSigFitnessVals &const_vpre_sig_fitness_vals,
                     bool search_phase,
                     SigBuffers &buffers,
                     ResetStateFuncType reset_state_neur,
                     GetVFuncType get_v_neur,
                     size_t ind_start_i,
                     size_t vpre_sig_start_i,
-                    size_t avg_smooth_points_model,
+                    size_t avg_smooth_points,
                     bool use_ifast,
                     bool use_islow);
 
