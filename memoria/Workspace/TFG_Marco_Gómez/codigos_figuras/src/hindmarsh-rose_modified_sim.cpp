edb2dbf58f86fd100f5ce36ccbd58b5209c30d43
Marco Gomez Hernandez <marco.gomezh@estudiante.uam.es>
2026-04-21 00:19:22 +0200

    a

--- memoria/Workspace/TFG_Marco_Gómez/codigos_figuras/src/hindmarsh-rose_modified_sim.cpp
+++ memoria/Workspace/TFG_Marco_Gómez/codigos_figuras/src/hindmarsh-rose_modified_sim.cpp
@@ -6,29 +6,15 @@
 #include <HindmarshRoseModel.h>
 #include <RungeKutta4.h>
 #include <SystemWrapper.h>
+#include "aux/utils.hpp"
 
 typedef RungeKutta4 Integrator;
 typedef DifferentialNeuronWrapper<SystemWrapper<HindmarshRoseModel<double>>, Integrator> HRNeuron;
 
 int main()
 {
-    HRNeuron::ConstructorArgs args;
-
-    args.params[HRNeuron::e] = 3.281;
-    args.params[HRNeuron::mu] = 0.0021;
-    args.params[HRNeuron::S] = 1.0;
-    args.params[HRNeuron::a] = 1.0;
-    args.params[HRNeuron::b] = 3.0;
-    args.params[HRNeuron::c] = 1.0;
-    args.params[HRNeuron::d] = 5.0;
-    args.params[HRNeuron::xr] = -1.6;
-    args.params[HRNeuron::vh] = 0.1;
-
-    HRNeuron n(args);
-
-    n.set(HRNeuron::x, -0.712841);
-    n.set(HRNeuron::y, -1.93688);
-    n.set(HRNeuron::z, 3.16568);
+    HRNeuron n = create_hindmarsh_rose<Integrator>(false);
+    reset_state_hindmarsh_rose(n);
 
     const double step = 0.001; // Paso de integración (estabilidad numérica)
     const int subsample = 100; // Guarda 1 de cada 100 iteraciones
